88f9e803d81358a6c4ebad6e138b55bab9c65dc3
Rafael Amaral <amaral.avelar.filo@gmail.com>
2022-03-19 23:39:44 -0300

    tentando resolver bug da pagina explore

---
+++ styled/exploreStyle/ExploreLayout.jsx
@@ -0,0 +1,9 @@
+import Container from './styled'
+export default function ExploreLayout({ children }) {
+  return (
+    <Container>
+    { children }
+    </Container>
+
+  )
+}
---
+++ styled/exploreStyle/styled.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+  width: 360px;
+  height: 640px;
+  button {
+    border-radius: 10px;
+    color: white;
+    font-size: 18px;
+    height: 4rem;
+    margin: 2rem;
+    width: 15rem;
+  }
+  .explore-foods-btn {
+    margin-top: 6rem;
+  }
+  .link-explore {
+    margin: 30rem;
+    text-decoration: none;
+    color: white;
+  }
+  
+`;
+
+export default Container;
+
+// import styled from 'styled-components';
+
+// const Container = styled.div`
+//   background-color: #f2f2f2;
+//   div {
+//     p {
+//       text-align: justify;
+//       width: 90%;
+//       margin-left: 1rem;
+//     }
+//   }
+
+//   div {
+//     width: 100vw;
+
+//     iframe {
+//       margin-top: 2rem;
+//       margin-left: 3rem;
+//     }
+//   }
+
+//   button.start-recipe {
+//     background-color: #c8a276;
+//     bottom: 0vh;
+//     color: white;
+//     height: 3rem;
+//     font-family: Arial, Helvetica, sans-serif;
+//     font-size: 18px;
+//     text-transform: uppercase;
+//     position: fixed;
+//     width: 100vw;
+//     z-index: 1;
+//   }
+// `;
+
+// export default Container;
+
